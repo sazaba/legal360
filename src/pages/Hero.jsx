d19e2c9959d7285321215ac40151cc089f32d9f0
Santiago Zappa <santiagozappab@gmail.com>
2025-05-26 21:06:00 -0500

    card text 222

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -27,14 +27,14 @@ const Hero = () => {
       {/* Contenido */}
       <div className="relative z-10 w-full min-h-screen flex items-center justify-center md:justify-start px-4 sm:px-6 md:px-12">
         <div className="text-center md:text-left max-w-2xl">
-          <h1 className="text-white text-3xl sm:text-4xl md:text-4xl lg:text-6xl font-bold mb-6 leading-tight md:leading-15 font-montserrat">
+          <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-6 leading-tight md:leading-tight text-center md:text-left font-montserrat px-4">
             Cumple con la Ley{' '}
             <span className="bg-gradient-to-r from-[#d4af37] via-[#f5e27a] to-[#d4af37] bg-clip-text text-transparent">
               Protege tu Empresa
             </span>
-            <br />
-            Crece con Legal 360
+            <span className="block">Crece con Legal 360</span>
           </h1>
+
 
           <p className="text-white text-base sm:text-lg md:text-xl mb-6 font-roboto">
             Legal, Claro y Eficiente
