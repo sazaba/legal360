7f07a18e365f1563067dd5d95fec2415c39c0696
Santiago Zappa <santiagozappab@gmail.com>
2025-05-26 19:23:21 -0500

    responsive3

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
       {/* Contenido */}
       <div className="relative z-10 w-full min-h-screen flex items-center justify-center md:justify-start px-4 sm:px-6 md:px-12">
         <div className="text-center md:text-left max-w-3xl">
-          <h1 className="text-white text-2xl sm:text-4xl md:text-5xl lg:text-7xl font-bold mb-6 leading-tight font-montserrat">
+          <h1 className="text-white text-3xl sm:text-4xl md:text-4xl lg:text-6xl font-bold mb-6 leading-tight font-montserrat">
             Cumple con la Ley{' '}
             <span className="bg-gradient-to-r from-[#d4af37] via-[#f5e27a] to-[#d4af37] bg-clip-text text-transparent">
               Protege tu Empresa
