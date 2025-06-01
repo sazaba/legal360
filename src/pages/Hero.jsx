ed0dd281a09dd10156adccc25669fd3221f0d6f3
Santiago Zappa <santiagozappab@gmail.com>
2025-06-01 13:40:22 -0500

    Formulario

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -22,14 +22,14 @@ const Hero = () => {
       }}
     >
       {/* Capa oscura para móvil */}
-      {isMobile && <div className="absolute inset-0 bg-black opacity-10 z-0" />}
+      {isMobile && <div className="absolute inset-0 bg-black opacity-20 z-0" />}
 
       {/* Contenido principal */}
       <div className="relative z-10 w-full min-h-screen flex items-center justify-center md:justify-start px-4 sm:px-8 md:px-20">
         <div className="text-center md:text-left max-w-2xl space-y-6">
 
           <div className="w-full max-w-6xl mx-auto px-6 py-12 flex flex-col items-center md:items-start text-center md:text-left gap-4">
-            <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-extrabold leading-tight font-montserrat tracking-tight">
+            <h1 className="text-white text-5xl sm:text-4xl md:text-5xl font-extrabold leading-tight font-montserrat tracking-tight">
               Cumple con la Ley{' '}
               <span
                 className="inline-block"
@@ -47,9 +47,8 @@ const Hero = () => {
 
             <div className="mt-10
              space-y-2">
-              <p className="text-[#1d1d1b] bg-[#e6d769] px-3 py-2 inline-block rounded-xl font-semibold text-base sm:text-lg shadow-lg hover:scale-105 transition-transform duration-300 animate-bounce">
-                Acompañamiento mensual o por evento
-              </p>
+              <p className="text-center text-[#001e33] bg-white px-3 py-2 inline-block rounded-xl font-semibold text-base sm:text-lg shadow-lg hover:scale-105 transition-transform duration-300 animate-bounce">
+                Acompañamiento Mensual <br /> Ó Por Evento!              </p>
             </div>
           </div>
 
